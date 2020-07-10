66caf2ff3d4acf2ed90abad5a0722e8b89c2b8f9
Sarah Elias <sarah@Sarahs-MacBook-Pro.local>
2020-07-10 10:27:42 -0700

    oop1 done

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,37 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+class Vehicle:
+    def __init__(self, name):
+
+
+class GroundVehicle(Vehicle):
+    def __init__(self, name, gas_type):
+        super().__init__(name)
+        self.gas_type = gas_type
+
+class Car(GroundVehicle):
+    def __init__(self, name, gas_type, make, model):
+        super().__init__(name, gas_type)
+        self.make = make
+        self.model = model
+
+class Motorcycle(GroundVehicle):
+    def __init__(self, name, gas_type, model):
+        super().__init__(name, gas_type)
+        self.model = model
+
+class FlightVehicle(Vehicle):
+    def __init__(self, name, engine_size):
+        super().__init__(name)
+        self.engine_size = engine_size
+
+class Airplane(FlightVehicle):
+    def __init__(self, name, engine_size, passenger_total):
+        super().__init__(name, engine_size)
+        self.passenger_total = passenger_total
+
+class Starship(FlightVehicle):
+    def __init__(self, engine_size, successful_flights):
+        super().__init__(name, engine_size)
+        self.successful_flights = successful_flights
